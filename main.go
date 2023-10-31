3bae8e50deddde2636e436246cfddefc794ac3b1
Eduardo Moraes Goncalves <eduardo.m.goncalves@unicuritiba.com.br>
2023-10-30 21:27:17 -0300

    testes

--- main.go
+++ main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var card string = "Ace of Spades"
+	card2 := "Text"
+	card2 = NewCard()
+
+	cards := []string{NewCard(), "Card 2", "Card 1", "Card N"}
+
+	fmt.Println(card)
+	fmt.Println(card2)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func NewCard() string {
+	return "Card sdfsdf"
+}
+
+func Cards(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n + Cards(n-1)
 }
