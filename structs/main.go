b37b88aff8cccf0778f10b747f5afcb02ab3d9ba
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-11-07 14:59:43 -0300

    Structs

---
+++ structs/main.go
@@ -0,0 +1,16 @@
+package main
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+
+}
