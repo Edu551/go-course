4f7c0b7adfacbb18f21e37adb02c68ac9af47160
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-11-07 21:55:58 -0300

    map syntax

--- map/main.go
+++ map/main.go
@@ -3,13 +3,23 @@ package main
 import "fmt"
 
 func main() {
+	//var colors map[string]string
+
+	// colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
+	// delete(colors, "white")
+
 	colors := map[string]string{
 		"red":   "#FF0000",
 		"green": "#008000",
 		"blue":  "#0000FF",
 	}
 
-	for i, color := range colors {
-		fmt.Println(i, color)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
 	}
 }
